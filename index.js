7c89ade5c73c132f615c58c352fe5ee36c7b59ff
Scott Nason <scottmnason@gmail.com>
2022-06-01 22:13:57 -0600

    commit tree ec17bf8205c3cac01c261c7a04c3b439ed45be2b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+function submitData (name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(response => response.json())
+    .then(newData => {
+        document.body.innerHTML = `<p>${newData.id}</p>`
+    })
+    .catch(resp => {
+        document.body.innerHTML = `<p>${resp.message}</p>`
+    })
+}
